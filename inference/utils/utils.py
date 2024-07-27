db22a1a2a69859879e3e1332072c1f2e6c484f64
Wei Xiong <90632760+WeiXiongUST@users.noreply.github.com>
2024-07-27 00:32:37 -0400

    Update utils.py

--- inference/utils/utils.py
+++ inference/utils/utils.py
@@ -1,11 +1,10 @@
-import os
 import json
+import os
 import random
-import json
-import os
-import numpy as np
 from pathlib import Path
-from typing import Iterable, Union, Any
+from typing import Any, Iterable, Union
+
+import numpy as np
 
 
 def set_seed(seed: int = 42) -> None:
@@ -36,20 +35,20 @@ def save_jsonl(samples, save_path):
     print("Saved to", save_path)
 
 
-def lower_keys(example):  
-    new_example = {}  
-    for key, value in example.items():  
-        if key != key.lower():  
-            new_key = key.lower()  
-            new_example[new_key] = value  
-        else:  
-            new_example[key] = value  
-    return new_example 
+def lower_keys(example):
+    new_example = {}
+    for key, value in example.items():
+        if key != key.lower():
+            new_key = key.lower()
+            new_example[new_key] = value
+        else:
+            new_example[key] = value
+    return new_example
 
 
 def construct_prompt(args, example):
-    if args.prompt_type == 'tora':
-        if 'gemma' in args.model_name_or_path: 
+    if args.prompt_type == "tora":
+        if "gemma" in args.model_name_or_path:
             full_prompt = f"<bos><start_of_turn>user\n{example['question']}<end_of_turn>\n<start_of_turn>model\n"
         elif "mistral" in args.model_name_or_path:
             full_prompt = f"<s> [INST] {example['question']} [/INST]"
@@ -57,9 +56,8 @@ def construct_prompt(args, example):
             full_prompt = f"User: {example['question']}\nPlease integrate natural language reasoning with programs to solve the problem above, and put your final answer within \\boxed{{}}.\n\nAssistant: "
         else:
             raise NotImplementedError(args.prompt_type + "and " + args.model_name_or_path)
-    elif args.prompt_type == 'cot':
-        #### TO DO
-        if 'gemma' in args.model_name_or_path: 
+    elif args.prompt_type == "cot":
+        if "gemma" in args.model_name_or_path:
             full_prompt = f"<bos><start_of_turn>user\n{example['question']}\nPlease reason step by step, and put your final answer within \\boxed{{}}.<end_of_turn>\n<start_of_turn>model\n"
         elif "mistral" in args.model_name_or_path:
             full_prompt = f"<s> [INST] {example['question']}\nPlease reason step by step, and put your final answer within \\boxed{{}}. [/INST]"
@@ -67,8 +65,9 @@ def construct_prompt(args, example):
             full_prompt = f"User: {example['question']}\nPlease reason step by step, and put your final answer within \\boxed{{}}.\n\nAssistant: "
         else:
             raise NotImplementedError(args.prompt_type + "and " + args.model_name_or_path)
-            
+
     return full_prompt
+
 
 key_map = {
     "gt": "Ground Truth",
@@ -77,26 +76,27 @@ key_map = {
     "score": "Score",
 }
 
+
 def show_sample(sample, print_all_preds=False):
-    print("=="*20)
+    print("==" * 20)
     for key in ["idx", "type", "level", "dataset"]:
         if key in sample:
             # capitalize
             print("{}: {}".format(key[0].upper() + key[1:], sample[key]))
-    print("Question:", repr(sample['question']))
-    if 'code' in sample:
+    print("Question:", repr(sample["question"]))
+    if "code" in sample:
         if print_all_preds:
-            for code in sample['code']:
-                print('-'*20)
+            for code in sample["code"]:
+                print("-" * 20)
                 print("code:", code)
-            print("Execution:", sample['report'])
+            print("Execution:", sample["report"])
         else:
-            print("Solution:\n", sample['code'][0])
-            print("Execution:", sample['report'][0])
-    if 'pred' in sample:
-        print("Prediction:", repr(sample['pred'][0]))
+            print("Solution:\n", sample["code"][0])
+            print("Execution:", sample["report"][0])
+    if "pred" in sample:
+        print("Prediction:", repr(sample["pred"][0]))
     for key in ["gt", "score", "unit", "gt_cot"]:
         if key in sample:
-            _key  = key_map.get(key, key)
+            _key = key_map.get(key, key)
             print("{}: {}".format(_key, repr(sample[key])))
     print()
