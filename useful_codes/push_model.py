f01f50cbc15e12e2643665f4790e0db02111e2db
WayXG <wexiong@google.com>
2024-07-27 05:00:33 -0400

    Update push_model.py

--- useful_codes/push_model.py
+++ useful_codes/push_model.py
@@ -1,8 +1,8 @@
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-name = '/home/wx/sft/RLHF-Reward-Modeling/pair-pm/pm_models/llama3-8b-it_bs64_lr5e6/checkpoint-1254'
-output_name = 'xxx/llama38b_5e6_sft3epoch'
+name = 'model_dir'
+output_name = 'output_name'
 tokenizer_name = name
 
 model = AutoModelForCausalLM.from_pretrained(
@@ -14,16 +14,3 @@ tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
 
 model.push_to_hub(output_name)
 tokenizer.push_to_hub(output_name)
-~                                                                                                                                                                                                                                                       
-~                                                                                                                                                                                                                                                       
-~                                                                                                                                                                                                                                                       
-~                                                                                                                                                                                                                                                       
-~                                                                                                                                                                                                                                                       
-~                                                                                                                                                                                                                                                       
-~                                                                                                                                                                                                                                                       
-~                                                                                                                                                                                                                                                       
-~                                                                                                                                                                                                                                                       
-~                                                                                                                                                                                                                                                       
-~                                                                                                                                                                                                                                                       
-~                                                                                                                                                                                                                                                       
-~                                                             
