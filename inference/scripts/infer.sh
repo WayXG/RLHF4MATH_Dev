77b81ff0c6a1730747e57f818ee02af93a784aa0
Wei Xiong <90632760+WeiXiongUST@users.noreply.github.com>
2024-07-27 01:14:52 -0400

    Update infer.sh

--- inference/scripts/infer.sh
+++ inference/scripts/infer.sh
@@ -1,20 +1,30 @@
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <model_path>"
+    exit 1
+fi
+MODEL_NAME_OR_PATH=$1
+
 DATA_NAME="gsm8k"
+
 OUTPUT_DIR="./output1"
 
-SPLIT="train"
+SPLIT="test"
 PROMPT_TYPE="tora"
-NUM_TEST_SAMPLE=100
-MODEL_NAME_OR_PATH=gemma
+NUM_TEST_SAMPLE=-1
 
-TOKENIZERS_PARALLELISM=false \
-python -um data_collect.infer_api_collect_data \
+
+CUDA_VISIBLE_DEVICES=0 TOKENIZERS_PARALLELISM=false \
+python -um infer_data.collect_data \
 --model_name_or_path ${MODEL_NAME_OR_PATH} \
 --data_name ${DATA_NAME} \
 --output_dir ${OUTPUT_DIR} \
 --split ${SPLIT} \
 --prompt_type ${PROMPT_TYPE} \
 --num_test_sample ${NUM_TEST_SAMPLE} \
---seed 0 \
---temperature 1.0 \
+--seed 1 \
+--temperature 0 \
 --n_sampling 1 \
---top_p 1 \                                      
+--top_p 1 \
+--start 0 \
+--end -1 \
+    
