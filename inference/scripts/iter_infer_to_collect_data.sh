cb039f70d184adaffe98496b22ab5a0c4be2fa6f
Wei Xiong <90632760+WeiXiongUST@users.noreply.github.com>
2024-07-28 11:40:40 -0400

    Update iter_infer_to_collect_data.sh

--- inference/scripts/iter_infer_to_collect_data.sh
+++ inference/scripts/iter_infer_to_collect_data.sh
@@ -6,7 +6,7 @@ MODEL_NAME_OR_PATH=$1
 
 #DATA_NAME="math"
 DATA_NAME="gsm8k"
-OUTPUT_DIR="./output1"
+OUTPUT_DIR="./iter_collect_data"
 
 SPLIT="train"
 PROMPT_TYPE="tora"
@@ -14,18 +14,27 @@ NUM_TEST_SAMPLE=-1
 
 for ((i=0; i<=8000; i+=1))
 do
-TOKENIZERS_PARALLELISM=false \
-python -um infer_data.collect_data \
+CUDA_VISIBLE_DEVICES=0 TOKENIZERS_PARALLELISM=false \
+python -um infer_data.infer_eval \
 --model_name_or_path ${MODEL_NAME_OR_PATH} \
 --data_name ${DATA_NAME} \
 --output_dir ${OUTPUT_DIR} \
 --split ${SPLIT} \
 --prompt_type ${PROMPT_TYPE} \
 --num_test_sample ${NUM_TEST_SAMPLE} \
---seed ${i} \
---temperature 1.0 \
+--seed 1 \
+--temperature 0 \
 --n_sampling 1 \
 --top_p 1 \
 --start 0 \
---end -1 
+--end -1 \
+--horizon 6 \
+--ports "8000" \
+--ports "8001" \
+--ports "8002" \
+--ports "8003" \
+--ports "8004" \
+--ports "8005" \
+--ports "8006" \
+--ports "8007" \
 done
